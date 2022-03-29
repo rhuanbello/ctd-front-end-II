fc36cd34fa7778160198531dd48f69f344cd0506
rhuanbello <rhuambello@gmail.com>
2022-03-29 20:51:15 -0300

    feat: Mesa de Trabalho Aula 16

---
+++ Aula 16/index.js
@@ -0,0 +1,371 @@
+const baseDeDados = {
+  "resultado": [
+    {
+      "genero": "masculino",
+      "nome": {
+        "titulo": "sr",
+        "primeiro": "Rhuam",
+        "utlimo": "Bello"
+      },
+      "localizacao": {
+        "rua": " Augusta , 238",
+        "cidade": "São Paulo",
+        "Estado": "São Paulo",
+        "postcode": "01305000",
+        "coordenadas": {
+          "latitude": "-23.5569581",
+          "longitude": "-46.6589677"
+        },
+        "timezone": {
+          "offset": "-3:00",
+          "descricao": "Brasil"
+        }
+      },
+      "email": "david.fernando@exemplo.com",
+      "login": {
+        "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
+        "usuario": "silverswan131",
+        "senha": "firewall",
+        "salt": "TQA1Gz7x",
+        "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
+        "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
+        "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
+      },
+      "dob": {
+        "data": "1993-07-20T09:44:18.674Z",
+        "idade": 26
+      },
+      "registro": {
+        "data": "2002-05-21T10:59:49.966Z",
+        "idade": 17
+      },
+      "fone": "011-4039-8745",
+      "celular": "011-99874-5621",
+      "id": {
+        "nome": "PPS",
+        "valor": "0390511T"
+      },
+      "imagem": {
+        "grande": "https://randomuser.me/api/portraits/men/75.jpg",
+        "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "IE"
+    },
+    {
+      "genero": "masculino",
+      "nome": {
+        "titulo": "sr",
+        "primeiro": "Michel",
+        "utlimo": "Picozzi"
+      },
+      "localizacao": {
+        "rua": " Augusta , 238",
+        "cidade": "São Paulo",
+        "Estado": "São Paulo",
+        "postcode": "01305000",
+        "coordenadas": {
+          "latitude": "-23.5569581",
+          "longitude": "-46.6589677"
+        },
+        "timezone": {
+          "offset": "-3:00",
+          "descricao": "Brasil"
+        }
+      },
+      "email": "david.fernando@exemplo.com",
+      "login": {
+        "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
+        "usuario": "silverswan131",
+        "senha": "firewall",
+        "salt": "TQA1Gz7x",
+        "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
+        "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
+        "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
+      },
+      "dob": {
+        "data": "1993-07-20T09:44:18.674Z",
+        "idade": 26
+      },
+      "registro": {
+        "data": "2002-05-21T10:59:49.966Z",
+        "idade": 17
+      },
+      "fone": "011-4039-8745",
+      "celular": "011-99874-5621",
+      "id": {
+        "nome": "PPS",
+        "valor": "0390511T"
+      },
+      "imagem": {
+        "grande": "https://randomuser.me/api/portraits/men/75.jpg",
+        "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "IE"
+    },
+    {
+      "genero": "masculino",
+      "nome": {
+        "titulo": "sr",
+        "primeiro": "Fabricio",
+        "utlimo": "Pereira"
+      },
+      "localizacao": {
+        "rua": " Augusta , 238",
+        "cidade": "São Paulo",
+        "Estado": "São Paulo",
+        "postcode": "01305000",
+        "coordenadas": {
+          "latitude": "-23.5569581",
+          "longitude": "-46.6589677"
+        },
+        "timezone": {
+          "offset": "-3:00",
+          "descricao": "Brasil"
+        }
+      },
+      "email": "david.fernando@exemplo.com",
+      "login": {
+        "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
+        "usuario": "silverswan131",
+        "senha": "firewall",
+        "salt": "TQA1Gz7x",
+        "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
+        "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
+        "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
+      },
+      "dob": {
+        "data": "1993-07-20T09:44:18.674Z",
+        "idade": 26
+      },
+      "registro": {
+        "data": "2002-05-21T10:59:49.966Z",
+        "idade": 17
+      },
+      "fone": "011-4039-8745",
+      "celular": "011-99874-5621",
+      "id": {
+        "nome": "PPS",
+        "valor": "0390511T"
+      },
+      "imagem": {
+        "grande": "https://randomuser.me/api/portraits/men/75.jpg",
+        "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "IE"
+    },
+    {
+      "genero": "masculino",
+      "nome": {
+        "titulo": "sr",
+        "primeiro": "Angel",
+        "utlimo": "Antonio"
+      },
+      "localizacao": {
+        "rua": " Augusta , 238",
+        "cidade": "São Paulo",
+        "Estado": "São Paulo",
+        "postcode": "01305000",
+        "coordenadas": {
+          "latitude": "-23.5569581",
+          "longitude": "-46.6589677"
+        },
+        "timezone": {
+          "offset": "-3:00",
+          "descricao": "Brasil"
+        }
+      },
+      "email": "david.fernando@exemplo.com",
+      "login": {
+        "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
+        "usuario": "silverswan131",
+        "senha": "firewall",
+        "salt": "TQA1Gz7x",
+        "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
+        "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
+        "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
+      },
+      "dob": {
+        "data": "1993-07-20T09:44:18.674Z",
+        "idade": 26
+      },
+      "registro": {
+        "data": "2002-05-21T10:59:49.966Z",
+        "idade": 17
+      },
+      "fone": "011-4039-8745",
+      "celular": "011-99874-5621",
+      "id": {
+        "nome": "PPS",
+        "valor": "0390511T"
+      },
+      "imagem": {
+        "grande": "https://randomuser.me/api/portraits/men/75.jpg",
+        "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "IE"
+    },
+    {
+      "genero": "masculino",
+      "nome": {
+        "titulo": "sr",
+        "primeiro": "Tiago",
+        "utlimo": "Guerreiro"
+      },
+      "localizacao": {
+        "rua": " Augusta , 238",
+        "cidade": "São Paulo",
+        "Estado": "São Paulo",
+        "postcode": "01305000",
+        "coordenadas": {
+          "latitude": "-23.5569581",
+          "longitude": "-46.6589677"
+        },
+        "timezone": {
+          "offset": "-3:00",
+          "descricao": "Brasil"
+        }
+      },
+      "email": "david.fernando@exemplo.com",
+      "login": {
+        "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
+        "usuario": "silverswan131",
+        "senha": "firewall",
+        "salt": "TQA1Gz7x",
+        "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
+        "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
+        "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
+      },
+      "dob": {
+        "data": "1993-07-20T09:44:18.674Z",
+        "idade": 26
+      },
+      "registro": {
+        "data": "2002-05-21T10:59:49.966Z",
+        "idade": 17
+      },
+      "fone": "011-4039-8745",
+      "celular": "011-99874-5621",
+      "id": {
+        "nome": "PPS",
+        "valor": "0390511T"
+      },
+      "imagem": {
+        "grande": "https://randomuser.me/api/portraits/men/75.jpg",
+        "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "IE"
+    },
+    {
+      "genero": "masculino",
+      "nome": {
+        "titulo": "sr",
+        "primeiro": "Murillo",
+        "utlimo": "Paulino"
+      },
+      "localizacao": {
+        "rua": " Augusta , 238",
+        "cidade": "São Paulo",
+        "Estado": "São Paulo",
+        "postcode": "01305000",
+        "coordenadas": {
+          "latitude": "-23.5569581",
+          "longitude": "-46.6589677"
+        },
+        "timezone": {
+          "offset": "-3:00",
+          "descricao": "Brasil"
+        }
+      },
+      "email": "david.fernando@exemplo.com",
+      "login": {
+        "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
+        "usuario": "silverswan131",
+        "senha": "firewall",
+        "salt": "TQA1Gz7x",
+        "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
+        "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
+        "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
+      },
+      "dob": {
+        "data": "1993-07-20T09:44:18.674Z",
+        "idade": 26
+      },
+      "registro": {
+        "data": "2002-05-21T10:59:49.966Z",
+        "idade": 17
+      },
+      "fone": "011-4039-8745",
+      "celular": "011-99874-5621",
+      "id": {
+        "nome": "PPS",
+        "valor": "0390511T"
+      },
+      "imagem": {
+        "grande": "https://randomuser.me/api/portraits/men/75.jpg",
+        "media": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "miniatura": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "IE"
+    },
+  ],
+  "info": {
+    "seed": "fea8be3e64777240",
+    "resultado": 1,
+    "pagina": 1,
+    "versao": "1.3"
+  }
+};
+
+const consultandoBaseDeDados = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (baseDeDados) {
+      resolve(baseDeDados);
+    } else {
+      reject({
+        "mensagem": "Base de dados inexistente."
+      });
+    }
+  });
+});
+
+consultandoBaseDeDados
+  .then(({ resultado }) => { // Then -> promisse retornou Sucesso
+    renderizarDadosUsuario(resultado)
+  }).catch((err) => { // promisse retornou Erro
+    
+  })
+
+const renderizarDadosUsuario = (data) => {
+  console.log('dados', data)
+
+  const container = document.querySelector('main');
+
+  data.forEach(usuario => {
+    container.innerHTML += `
+    <div class="target">
+      <img src=${usuario.imagem.grande}>
+      <div class="paragraphs">
+        <div>
+          <p>Nome</p>
+          <p>${usuario.nome.titulo}. ${usuario.nome.primeiro} ${usuario.nome.utlimo}</p>
+        </div>
+        <div>
+          <p>Celular</p>
+          <p>${usuario.celular}</p>
+        </div>
+        <div>
+          <p>E-mail</p>
+          <p>${usuario.email}</p>
+        </div>
+      </div>
+    </div>
+  `
+  })
+
+}
+
