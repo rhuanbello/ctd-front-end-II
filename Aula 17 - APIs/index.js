47c04546f22c2264a09628fd0cd46ff166e1c617
rhuanbello <rhuambello@gmail.com>
2022-03-30 22:48:42 -0300

    feat: Mesa de Trabalho 17 - APIs

--- Aula 17 - APIs/index.js
+++ Aula 17 - APIs/index.js
@@ -1,15 +1,11 @@
 const inputResults = document.querySelector('input')
 const searchButton = document.getElementById('search')
 
-const url = 'https://randomuser.me/api/?results=10'
-
 searchButton.addEventListener('click', (e) => {
   e.preventDefault()
   const search = inputResults.value
   getUsers(search)
 })
-
-// Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
 
 const getUsers = (search) => {
 
@@ -55,8 +51,3 @@ const renderizarDadosUsuario = (data) => {
   })
 
 }
-
-/* --------------------------- Tarefa 2 (extra) --------------------------- */
-// Aqui você pode ir para o ponto extra de usar o botão que está comentado no HTML.
-// Você pode descomentar o código no index.html e usar esse botão para executar uma nova solicitação API, sem recarregar a página.
-// Cabe aos desenvolvedores decidirem qual bloco de código deve ser contido dentro de uma função para que ele possa ser executado toda vez que um clique de botão for realizado.
